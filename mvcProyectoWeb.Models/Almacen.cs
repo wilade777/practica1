5cfc0f41097c2f5f40610a3377ea5604f1cb5c18
wilade777 <51494119+wilade777@users.noreply.github.com>
2024-04-17 18:44:23 -0400

    Add files via upload

---
+++ mvcProyectoWeb.Models/Almacen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mvcProyectoWeb.Models
+{
+    public class Almacen
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Ingrese el nombre del almacen")]
+        [Display(Name = "Nombre del Almacen")]
+        public string NombreAlmacen { get; set; }
+        [Required(ErrorMessage = "La direccion es obligatoria")]
+        public string Direccion { get; set; }
+        [DataType(DataType.ImageUrl)]
+        [Display(Name = "Imagen")]
+        public string UrlImagen { get; set; }
+        [Display(Name = "Orden de Visualización")]
+        [Range(1, 100, ErrorMessage = "El valor debe estar entre 1 y 100")]
+        public int Orden { get; set; }
+
+    }
+}
